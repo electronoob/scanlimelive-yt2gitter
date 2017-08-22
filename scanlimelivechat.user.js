a72938fc8b23c9737aefc2b6f5c3a4606e5e68e3
electronoob <electronoob@electronoob.com>
2017-08-22 01:54:18 +0100

    added userscript

---
+++ scanlimelivechat.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         scanlime live-stream gitter embedderer
+// @namespace    http://electronoob.com/
+// @version      0.1
+// @description  fanboi script for replacing yt live chat with gitter.im on micahs live streams only.
+// @author       electronoob
+// @match        https://*.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    if("Micah Elizabeth Scott" === document.evaluate('//*[@id="watch7-user-header"]/div/a', document, null, XPathResult.ANY_TYPE, null ).iterateNext().innerText)
+        document.getElementById('live-chat-iframe').src="https://gitter.im/scanlime/live";
+})();
